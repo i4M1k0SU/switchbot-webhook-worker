8371e408edb32ff4546d9a7bc16c8aab3eb3f85c
i4M1k0SU <i4m1k0su@i4m1k0.su>
2025-06-27 12:45:45 +0900

    add deviceMac check

--- src/handlers/MailboxMotionSensor.ts
+++ src/handlers/MailboxMotionSensor.ts
@@ -3,7 +3,7 @@ import type { DeviceWebhook } from '../types/SwitchBot';
 import { discordWebhook } from '../functions/Discord';
 
 export async function handle(event: DeviceWebhook, env: MyEnv): Promise<void> {
-	if (event.context.deviceType !== 'WoPresence') {
+	if (event.context.deviceType !== 'WoPresence' || event.context.deviceMac !== env.MAILBOX_MOTION_SENSOR_MAC) {
         return;
     }
 
@@ -12,7 +12,7 @@ export async function handle(event: DeviceWebhook, env: MyEnv): Promise<void> {
 			env.DISCORD_WEBHOOK_URL,
 			'郵便受け',
 			'開かれました',
-      'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiuVEr-cnrFixh2J35I6aI1_qaQRWlXe_lcKr1UV9vuxrnAsbSwQxSU5JnoMLyb80bXvc8tkNIq37qzFTdVZq9oW8AZZiH0FsU49G4AGBtdT3xYHpjEfC8iBfxt5mWb5IesxXVzBDRu5EI/s800/yuubin_mail_bin.png'
+      'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiuVEr-cnrFixh2J35I6aI1_qaQRWlXe_lcKr1UV9vuxrnAsbSwQxSU5JnoMLyb80bXvc8tkNIq37qzFTdVZq9oW8AZZiH0FsU49G4AGBtdT3xYHpjEfC8iBfxt5mWb5IesxXVzBDRu5EI/s800/yuubin_mail_bin.png',
 		);
 	}
 }
--- src/types/Env.ts
+++ src/types/Env.ts
@@ -1,6 +1,7 @@
 // npx wrangler secret put [KEY]
 export type SecretEnv = {
   DISCORD_WEBHOOK_URL: string;
+  MAILBOX_MOTION_SENSOR_MAC: string;
 };
 
 export type MyEnv = Env & SecretEnv;
